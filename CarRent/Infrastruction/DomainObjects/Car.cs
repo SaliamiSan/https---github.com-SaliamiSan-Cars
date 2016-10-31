c279f165e11041e17e522afeb2e9dfeb1b0a6fc0
Alexander43391122 <Alexander43391122@users.noreply.github.com>
2016-10-31 21:31:43 +0300

    Fix issue with Enum values

--- CarRent/Infrastruction/DomainObjects/Car.cs
+++ CarRent/Infrastruction/DomainObjects/Car.cs
@@ -4,11 +4,17 @@ namespace Infrastruction.DomainObjects
 {
     public class Car
     {
+        //[System.ComponentModel.DataAnnotations.Display(=false)]
         public int CarId { get; set; }
 
+        [Display(Name="Name of car")]
         public string CarName { get; set; }
 
-        //[UIHint("Enum")]
+        [Display(Name="Total count of car")]
+        [Range(0, 1000)]
+        public int TotalCount { get; set; }
+
+        [System.ComponentModel.DataAnnotations.EnumDataType(typeof(CarStatus))]
         public CarStatus Status { get; set; }
     }
 }
